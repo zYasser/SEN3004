137375af16a482e0d46154258c0fcaf09d5c8212
yasserqamhiah <yaserqamhiah@gmail.com>
2024-03-26 03:20:17 +0300

    week 3

---
+++ Week3/src/main/java/sen3004/app1/App1Application.java
@@ -0,0 +1,13 @@
+package sen3004.app1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App1Application.class, args);
+	}
+	
+}
